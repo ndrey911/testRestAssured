a561c7463ef188eeed58b7b61eba92cb1f9c9a99
gainullin <aagainullin@yandex.ru>
2022-10-31 00:13:27 +0400

    Added Base URI in BookerAPIUrls; Added response specification; Added new test - createBooking;

--- src/test/java/tests/testAPI/RESTfulBookerAPI/BookerAPIUrls.java
+++ src/test/java/tests/testAPI/RESTfulBookerAPI/BookerAPIUrls.java
@@ -3,6 +3,7 @@ package tests.testAPI.RESTfulBookerAPI;
 public final class BookerAPIUrls {
     private BookerAPIUrls(){}
 
+    public static final String BASE_URI = "https://restful-booker.herokuapp.com/";
     public static final String AUTH = "auth";
     public static final String BOOKING = "booking";
     public static final String PING = "ping";
--- src/test/java/tests/testAPI/RESTfulBookerAPI/RESTfulBookerAPI.java
+++ src/test/java/tests/testAPI/RESTfulBookerAPI/RESTfulBookerAPI.java
@@ -3,6 +3,7 @@ package tests.testAPI.RESTfulBookerAPI;
 
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -16,22 +17,50 @@ public class RESTfulBookerAPI {
     private String userName = "admin";
     private String pass = "password123";
     private String authToken;
+    private String bookingid;
 
-    RequestSpecification requstSpec = given()
-            .baseUri("https://restful-booker.herokuapp.com/")
+    RequestSpecification requestSpec = given()
+            .baseUri(BASE_URI)
             .contentType("application/json");
+
+    ResponseSpecification responseSpec = given()
+            .expect()
+            .statusCode(200);
 
     @Test(description = "Creates a new auth token")
     public void Auth(){
-        Response authResponse = given().spec(requstSpec)
+        Response authResponse = given().spec(requestSpec)
                 .body("{\"username\" : \""+userName+"\",\"password\" : \""+pass+"\"}")
                 .when()
                 .post(AUTH)
                 .then()
                 .extract().response();
-        authResponse.then().statusCode(200);
-        authResponse.then().body("token", notNullValue());
+        authResponse.then().spec(responseSpec)
+                .body("token", notNullValue());
         authToken = authResponse.path("token").toString();
+    }
+
+    @Test(description = "Creates a new booking in the API")
+    public void CreateBooking(){
+        Response createResponse = given().spec(requestSpec)
+                .body("{\n" +
+                        "    \"firstname\" : \"Jim\",\n" +
+                        "    \"lastname\" : \"Brown\",\n" +
+                        "    \"totalprice\" : 111,\n" +
+                        "    \"depositpaid\" : true,\n" +
+                        "    \"bookingdates\" : {\n" +
+                        "        \"checkin\" : \"2018-01-01\",\n" +
+                        "        \"checkout\" : \"2019-01-01\"\n" +
+                        "    },\n" +
+                        "    \"additionalneeds\" : \"Breakfast\"\n" +
+                        "}")
+                .when()
+                .post(BOOKING)
+                .then()
+                .extract().response();
+        createResponse.then().spec(responseSpec)
+                .body("bookingid", notNullValue());
+        bookingid = createResponse.path("bookingid").toString();
     }
 
 
